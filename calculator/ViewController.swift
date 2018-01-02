d863f4147d76c938ba7cb6985c6f5b203d478256
Vachirasuk Setalaphruk <a@example.com>
2018-01-02 22:12:08 +0700

    Initial version

--- calculator/ViewController.swift
+++ calculator/ViewController.swift
@@ -10,9 +10,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var labelFormula: UILabel!
+    @IBOutlet weak var labelResult: UILabel!
+    
+    @IBAction func numbers(_ sender: UIButton) {
+        labelResult.text = labelResult.text! + String(sender.tag - 30)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        labelResult.text = ""
+        labelFormula.text = ""
     }
 
     override func didReceiveMemoryWarning() {
